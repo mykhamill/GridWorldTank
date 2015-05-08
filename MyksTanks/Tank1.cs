196a01a4c5cb621f73d1c342b2933130e4f48c86
Myk <myk_github@mykat.co.uk>
2015-05-08 17:12:41 +0100

    Initial Commit

---
+++ MyksTanks/Tank1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace GridWorld
+{
+    public class mhamill: BasePlayer
+    {
+        PlayerWorldState myWorldState;
+
+        public mhamill() : base()
+        {
+            this.Name = "Tank1";
+        }
+
+        public override ICommand GetTurnCommands(IPlayerWorldState igrid)
+        {
+            myWorldState = (PlayerWorldState)igrid;
+            Debug.WriteLine(myWorldState);
+
+            return null; // Return the command “Do nothing at all!”
+        }
+
+        public GridSquare GetMyLocation(PlayerWorldState state)
+        {
+            Debug.WriteLine(state.MyGridSquare);
+            return null;
+        }
+    }
+}
