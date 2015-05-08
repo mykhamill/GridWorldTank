70c49bb906dafa69b6b7168c0f563bf0d09064f4
Myk <myk_github@mykat.co.uk>
2015-05-08 23:51:05 +0100

    move Memory to seperate file

---
+++ MyksTanks/Memory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GridWorld
+{
+    class Memory
+    {
+        Dictionary<Point, GridSquare> mem;
+        int maxWidth;
+        int maxHeight;
+
+        public Memory()
+        {
+            mem = new Dictionary<Point, GridSquare>();
+            maxWidth = 0;
+            maxHeight = 0;
+        }
+
+        public String convertContent(GridSquare.ContentType ct)
+        {
+            switch(ct)
+            {
+                case GridSquare.ContentType.DestroyedTank: return "#";
+                case GridSquare.ContentType.Empty: return ".";
+                case GridSquare.ContentType.Rock: return "*";
+                case GridSquare.ContentType.TankDown: return "v";
+                case GridSquare.ContentType.TankLeft: return "<";
+                case GridSquare.ContentType.TankRight: return ">";
+                case GridSquare.ContentType.TankUp: return "^";
+                default: return " ";
+            }
+        }
+
+        public void update(PlayerWorldState state)
+        {
+            maxWidth = state.GridWidthInSquares;
+            maxHeight = state.GridHeightInSquares;
+            var s = state.MyVisibleSquares;
+            foreach (var sq in s)
+            {
+                var p = new Point(sq.X, sq.Y);
+                mem[p] = sq;
+            }
+        }
+
+        public override string ToString()
+        {
+            var result = new List<String>();
+            for (var y = 0; y < maxHeight; y++)
+            {
+                result.Add("");
+                for (var x = 0; x < maxWidth; x++)
+                {
+                    var p = new Point(x, y);
+                    if (mem.ContainsKey(p))
+                    {
+                        result[result.ToArray().Length-1] += convertContent(mem.SingleOrDefault(kv => kv.Key == p).Value.Contents);
+                    }
+                    else
+                    {
+                        result[result.ToArray().Length - 1] += " ";
+                    }
+                }
+            }
+            return result.Aggregate((a, b) => b + "\r\n" + a);
+        }
+    }
+}
--- MyksTanks/Tank1.cs
+++ MyksTanks/Tank1.cs
@@ -10,63 +10,26 @@ namespace GridWorld
     public class mhamill: BasePlayer
     {
         PlayerWorldState myWorldState;
-        Dictionary<Point, GridSquare> memory;
+        Memory memory;
 
         public mhamill() : base()
         {
             this.Name = "Tank1";
-            memory = new Dictionary<Point, GridSquare>();
+            memory = new Memory();
         }
 
         public override ICommand GetTurnCommands(IPlayerWorldState igrid)
         {
             myWorldState = (PlayerWorldState)igrid;
-            updateMemory(myWorldState);
-            Debug.WriteLine(memoryToString(memory));
+            memory.update(myWorldState);
+            Debug.WriteLine(memory);
             Debug.WriteLine(myWorldState);
             mloc();
             findEnemies(myWorldState);
             Trace.WriteLine(String.Format("Move {0}, Fire? {1}", Command.Move.Right, false ? "Yes" : "No"));
             return new Command(Command.Move.Right, false);
         }
-
-        public void updateMemory(PlayerWorldState state)
-        {
-            foreach (var square in state.MyVisibleSquares.ToList())
-            {
-                var s = new Point(square.X, square.Y);
-                if (memory.ContainsKey(s))
-                {
-                    memory.Remove(s);
-                }
-                memory.Add(s, new GridSquare(square.X, square.Y, square.Contents, square.Player));
-            }
-        }
-
-        public String memoryToString(Dictionary<Point, GridSquare> mem)
-        {
-            String result = "";
-            var mx = this.myWorldState.GridWidthInSquares;
-            var my = this.myWorldState.GridHeightInSquares;
-            for (var y = 0; y < my; y++)
-            {
-                for (var x = 0; x < mx; x++)
-                {
-                    var p = new Point(x, y);
-                    if (mem.ContainsKey(p))
-                    {
-                        result += mem.SingleOrDefault(kv => kv.Key == p).Value.ContentString;
-                    }
-                    else
-                    {
-                        result += " ";
-                    }
-                }
-                result += "\r\n";
-            }
-            return result;
-        }
-
+        
         public GridSquare mloc()
         {
             if (myWorldState != null)
