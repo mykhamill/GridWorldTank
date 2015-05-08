e962e84dd26050fa62ac494cfb5f89f90133ccf6
Myk <myk_github@mykat.co.uk>
2015-05-08 20:59:22 +0100

    add findEnemies

--- MyksTanks/Tank1.cs
+++ MyksTanks/Tank1.cs
@@ -19,15 +19,30 @@ namespace GridWorld
         {
             myWorldState = (PlayerWorldState)igrid;
             Debug.WriteLine(myWorldState);
-
+            mloc();
+            findEnemies(myWorldState);
+            Trace.WriteLine(String.Format("Move {0}, Fire? {1}", Command.Move.Right, false ? "Yes" : "No"));
             return new Command(Command.Move.Right, false);
-            //return null; // Return the command “Do nothing at all!”
         }
 
-        public GridSquare GetMyLocation(PlayerWorldState state)
+        public GridSquare mloc()
         {
-            Debug.WriteLine(state.MyGridSquare);
+            if (myWorldState != null)
+            {
+                Debug.WriteLine(myWorldState.MyGridSquare);
+                return myWorldState.MyGridSquare;
+            }
             return null;
+        }
+
+        public List<GridSquare> findEnemies(PlayerWorldState state)
+        {
+            var visibleEnemySquares = state.MyVisibleSquares.Where(square => square.Player > 0 && square.Player != this.ID).ToList();
+            foreach (var square in visibleEnemySquares)
+            {
+                Debug.WriteLine(String.Format("Player: {0} at {1}", square.Player, square));
+            }
+            return visibleEnemySquares;
         }
     }
 }
